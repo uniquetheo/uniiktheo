07926c886e75b51152b8cf04b613bdf345ea2ed7
uniquetheo <55855459+uniquetheo@users.noreply.github.com>
2025-02-28 15:56:30 +0000

    footer icon to primary color

--- components/ui/myTools.jsx
+++ components/ui/myTools.jsx
@@ -2,11 +2,24 @@
 
 import { tools } from "@/lib/data";
 import Image from "next/image";
-import React from "react";
+import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
 const MyTools = () => {
   const { theme } = useTheme();
+  const [githubLogo, setGithubLogo] = useState("/images/github.svg");
+  const [vercelLogo, setVercelLogo] = useState("/images/vercel.svg");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setGithubLogo("/images/github.dark.svg");
+      setVercelLogo("/images/vercel.dark.svg");
+    } else {
+      setGithubLogo("/images/github.svg");
+      setVercelLogo("/images/vercel.svg");
+    }
+  }, [theme]);
+
   return (
     <>
       {tools.map((tool, index) => (
@@ -15,12 +28,13 @@ const MyTools = () => {
           className="w-[60px] h-[60px] border relative rounded shadow p-4 hover:scale-105"
         >
           <Image
-            // src={
-            //   theme === "dark" && tool.name === "github"
-            //     ? "github.dark.svg"
-            //     : `${tool.imgSrc}`
-            // }
-            src={tool.imgSrc}
+            src={
+              tool.name === "github"
+                ? githubLogo
+                : tool.name === "vercel"
+                ? vercelLogo
+                : tool.imgSrc
+            }
             alt={tool.name}
             fill
             className="p-2"
